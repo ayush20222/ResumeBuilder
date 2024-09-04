4c8965ccb08f4e396f7ddbd890bb8c3b43c11d66
ayush20222 <99115140+ayush20222@users.noreply.github.com>
2024-09-04 12:28:19 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -13,7 +13,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            return redirect('resume')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
